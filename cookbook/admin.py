442711b89ecd1d71c685d73c67b9ccd52f46cf04
KaiLangen96 <>
2023-07-22 16:19:17 +0000

    Migrate changes

--- cookbook/admin.py
+++ cookbook/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Recipe, Category, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('category', 'admin')
+
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'created_on')
+    search_fields = ('title', 'content')
+    list_filter = ('created_on', 'updated_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'recipe', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- cookbook/models.py
+++ cookbook/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Category(models.Model):
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=70, unique=True)
+    slug = models.SlugField(max_length=70, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='recipes')
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='category')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
