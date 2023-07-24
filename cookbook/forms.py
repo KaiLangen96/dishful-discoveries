a550e44d10ee6e430460e18c00ee06952bf70fe7
KaiLangen96 <>
2023-07-24 18:13:09 +0000

    Fix render issue: recipe form as crispy form

--- cookbook/forms.py
+++ cookbook/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, Recipe
 from django import forms
 
 
@@ -6,3 +6,16 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = [
+            'title',
+            'description',
+            'method',
+            'image',
+            'is_vegetarian',
+            'is_vegan'
+        ]
