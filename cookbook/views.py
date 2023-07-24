16ba4a71c259c45425fe764b8e0a9be5d228402e
KaiLangen96 <>
2023-07-24 17:35:54 +0000

    Remove approval & Update recipe_detail.html & Add POST for comments

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Recipe
+from .forms import CommentForm
 
 
 class RecipeList(generic.ListView):
@@ -15,7 +16,7 @@ class RecipeDetail(View):
     def get(self, request, slug, *args, **kwargs):
         # Recipe might be the wrong argument:
         recipe = get_object_or_404(Recipe, slug=slug)
-        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        comments = recipe.comments.order_by('created_on')
         # Add Like filter
 
         return render(
@@ -24,8 +25,39 @@ class RecipeDetail(View):
             {
                 "recipe": recipe,
                 "comments": comments,
+                "comment_form": CommentForm(),
                 # "likes": likes,
-            }
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.order_by('created_on')
+        # favourite = False
+        # if recipe.favourites.filter
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.recipe = recipe
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                # "commented": True,
+                "comment_form": CommentForm()
+                # "favourite": favourite
+            },
         )
 
 
