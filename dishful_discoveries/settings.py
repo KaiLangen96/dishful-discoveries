242f1805046bfaca268957baa84a2f01fe85045c
KaiLangen96 <>
2023-08-01 16:07:10 +0000

    Set DEBUG to True

--- dishful_discoveries/settings.py
+++ dishful_discoveries/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-kailangen96-dishful-disc-95kna0uohp.us2.codeanyapp.com',
